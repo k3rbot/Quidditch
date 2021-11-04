9590757f40a82da7dc44052682de568e28bcf282
k3rbot <marty.piton@gmail.com>
2021-11-04 16:23:35 +0100

    Simulation

--- simulation.py
+++ simulation.py
@@ -1,19 +1,11 @@
 from random import randint
-pointvifdor = 0
-pointvifdor2 = 0
-while pointvifdor != 25 and pointvifdor2 != 25 :
-    pointball = randint(1,3)
-    if pointball == 2:
-        pointball =10
-    pointvifdor = randint(1,50)
-    if pointvifdor == 25:
-        pointvifdor = 150
-    point_equipe_1 = pointball + pointvifdor 
-    pointball2 = randint(1,3)
-    if pointball2 == 2:
-        pontball2 = 10
-    pointvifdor2 = randint(1,50)
-    if pointvifdor2==25:
-        pointvifdor2=150
-    point_equipe_2 = pointball2 + pointvifdor2
-print(f"lequipe 1 a marqué {point_equipe_1},et l'equipe 2 a marqué {point_equipe_2}")
+points = [0, 0]
+vifdor = False
+while not(vifdor):
+    for equipe in range(2):
+        if randint(1,3) == 2:
+            points[equipe] += 10
+        if randint(1,50) == 25:
+            points[equipe] = 150
+            vifdor = True
+print(f"lequipe 1 a marqué {points[0]}, et l'equipe 2 a marqué {points[1]}")
