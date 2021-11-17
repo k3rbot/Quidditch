9bb79b668c7a07ffb3ef3fc9f561a22c8b2991db
k3rbot <marty.piton@gmail.com>
2021-11-17 17:02:05 +0100

    Modified Gilda's code + added sorting ability

--- persos.py
+++ persos.py
@@ -1,14 +1,21 @@
 projet_tab = []
+nb = 0 
+
 with open("Characters.csv", mode='r', encoding='utf-8') as f:
     lines = f.readlines()
     key_line = lines[0].strip()
     keys = key_line.split(";")
-    for line in lines[1:]:
+    for line in lines[1:5]:
         line = line.strip()
         values = line.split(';')
         dico = {}
         for i in range(len(keys)):
-            dico[keys[i]] = values[i]
+            if i != 0:
+                dico[keys[i]] = values[i]
+            else:
+                dico["Points"] = 0
         projet_tab.append(dico)
     
 print(projet_tab)
+
+print(sorted(projet_tab, key=lambda x: x["Points"], reverse = True))
